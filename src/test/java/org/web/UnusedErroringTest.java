33be21458962fd329ebcf7013b1e9406671d99cb
Sidhavratha <sidhavratha@gmail.com>
2014-06-15 01:04:08 +0530

    Changes

---
+++ src/test/java/org/web/UnusedErroringTest.java
@@ -0,0 +1,17 @@
+package org.web;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by Sidhavratha on 15/6/14.
+ */
+public class UnusedErroringTest {
+
+    @Test
+    public void testError()
+    {
+        Assert.assertFalse(true);
+    }
+
+}
